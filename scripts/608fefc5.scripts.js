a6a489f3484e7888b8e18ffd7f2dd112ef14c448
srasmusson <rasmusson.stefan@gmail.com>
2014-03-04 19:50:51 +0100

    Updates

---
+++ scripts/608fefc5.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("fusionTablesToolApp",[]),angular.module("fusionTablesToolApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
